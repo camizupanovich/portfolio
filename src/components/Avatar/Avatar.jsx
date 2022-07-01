189c0755027b908605532d82d1c460e08252c2b2
Camila Zupanovich <cami.zupanovich@hotmail.com>
2022-07-01 19:31:49 -0300

    bg,about

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -6,8 +6,11 @@ export default function Avatar(){
     return(
         <div className={s.container}>
             <div className={s.box}>
-                <img src={avatar} alt='full stack' width='300px'/>
-                <h2 className={s.welcome}>Hi! I'm Camila Zupanovich!</h2>
+                    <div className={s.welcome}>Hi! I'm Camila!</div>
+                    <div className={s.name}>Full Stack Developer</div>
+                <img className={s.avatar} src={avatar} alt='full stack' width='300px'/>
+                <div className={s.about}><p className={s.aboutP}>"A young developer from Buenos Aires,Argentina 🇦🇷. As passionate about design and logic, she makes each project unique and innovative where she pours all her creativity and enthusiasm. She defines herself as a full stack developer in constant learning as a symbol of updating in a changing world."</p></div>
+                <button>Resume</button>
             </div>
         </div>
     )
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -11,15 +11,15 @@ export default function Landing(){
     return(
         <>  
         <div className={s.appBar}>
-            <a href='#1' className={s.item}>Skills</a>
-            <a href='#2' className={s.item}>\Projects</a>
-            <a href='#3' className={s.item}>\Certificate</a>
+            <a href='#skills' className={s.item}>Skills</a>
+            <a href='#projects' className={s.item}>Projects</a>
+            <a href='#certifications' className={s.item}>Certificate</a>
         </div>
             <Nav/>
             <section><Avatar/></section>
-            <section id='1'><Skills/></section>
-            <section id='2'><Projects/></section>
-            <section id='3'><Certifications/></section>
+            <section id='skills'><Skills/></section>
+            <section id='projects'><Projects/></section>
+            <section id='certifications'><Certifications/></section>
         </>
     )
 }

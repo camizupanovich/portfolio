6fcca7f76a94732f70fe479eb5b7d4f7c3b623c7
unknown <camizupa.absti@gmail.com>
2023-08-24 14:54:36 -0300

    contact mail added

--- src/history/2023/Contact/Contact.jsx
+++ src/history/2023/Contact/Contact.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Button, TextField, Typography, MenuItem, Select, InputLabel,FormControl } from "@mui/material";
-import colors from "../helpers/colors";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { countries } from "../helpers/countries";
+import Swal from 'sweetalert2'; 
 export default function Contact({ data, backgroundColorPrimary, textColor, fontColor }) {
     const theming = createTheme({
         palette: {
@@ -11,14 +11,48 @@ export default function Contact({ data, backgroundColorPrimary, textColor, fontC
             }
         },
     });
-    const [selectedCountry, setSelectedCountry] = useState('')
-    const handleChange = (event) => {
-        console.log(event.target.value);
-        setSelectedCountry(event.target.value);
+    
+    const [form, setForm] = useState({name:'',email:'',country:'',message:''})
+    const handleChange = (e) => {
+        setForm({...form,[e.target.name]:e.target.value});
     };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        var data = new FormData(event.target)
+        try {
+            const response = await fetch('https://formspree.io/f/xgejknll', {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Accept': 'application/json',
+                }
+            });
 
+            if (response.ok) {
+                Swal.fire({
+                    icon: 'success',
+                    title: data.contact.alert.success.title,
+                    text: data.contact.alert.success.subtitle,
+                });
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: data.contact.alert.error.title,
+                    text: data.contact.alert.error.subtitle,
+                    footer:data.contact.alert.error.subtitle2
+                });
+            }
+        } catch (error) {
+            Swal.fire({
+                icon: 'error',
+                title: data.contact.alert.error.title,
+                text: data.contact.alert.error.subtitle,
+                footer:data.contact.alert.error.subtitle2
+            });
+        }
+    };
     return (
-        <div style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: backgroundColorPrimary, padding: ' 10vh 10vw', }}>
+        <form onSubmit={handleSubmit} method="POST" style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: backgroundColorPrimary, padding: ' 10vh 10vw', }}>
             {/* {countries.map((e, i) => <div key={i}><p>{e.name}</p><img src={e.flag} alt={e.name} /></div>)} */}
             <ThemeProvider theme={theming}>
                 <div style={{ maxWidth:'700px'}}>
@@ -26,13 +60,18 @@ export default function Contact({ data, backgroundColorPrimary, textColor, fontC
                         {data.contact.title}
                     </Typography>
                     <TextField 
+                    name="name"
                         id="outlined-multiline-flexible"
                         label={data.contact.label[0]}
+                        onChange={(e)=>handleChange(e)}
                         sx={{ mt: 2, color: fontColor }}
                         variant="outlined" fullWidth
                         required
                     />
                     <TextField
+                    name="email"
+                    value={form['email']}
+                    onChange={(e)=>handleChange(e)}
                         id="outlined-multiline-flexible"
                         label={data.contact.label[1]}
                         sx={{ mt: 2, color: fontColor }}
@@ -46,8 +85,9 @@ export default function Contact({ data, backgroundColorPrimary, textColor, fontC
                         <Select
                             labelId="demo-controlled-open-select"
                             id="demo-controlled"
-                            value={selectedCountry}
-                            onChange={handleChange}
+                            value={form['country']}
+                            name="country"
+                            onChange={(e)=>handleChange(e)}
                             label={data.contact.label[2]}
                         >
                             <MenuItem value="">
@@ -59,15 +99,18 @@ export default function Contact({ data, backgroundColorPrimary, textColor, fontC
                     </FormControl>
                     <TextField
                         id="outlined-multiline-flexible"
+                        name="message"
+                        value={form['message']}
+                        onChange={(e)=>handleChange(e)}
                         label={data.contact.label[3]}
                         multiline
                         rows={4} sx={{ mt: 2, color: fontColor }}
                         variant="outlined" fullWidth
                         required
                     />
-                    <Button sx={{ my: 3, width: '100%' }} variant="contained">{data.contact.button}</Button>
+                    <Button sx={{ my: 3, width: '100%' }} type="submit" variant="contained">{data.contact.button}</Button>
                 </div>
             </ThemeProvider>
-        </div>
+        </form>
     )
 }
